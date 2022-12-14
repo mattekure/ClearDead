7e956c57e2eab492af31ffd99a7e8c27ce71bf55
mattekure <585663+mattekure@users.noreply.github.com>
2022-12-13 22:19:13 -0500

    remove build.bat

--- build.bat
+++
@@ -1,3 +0,0 @@
-7z a "ClearDead.zip" *
-COPY "ClearDead.zip" "C:\Users\John\Fantasy Grounds Unity\channels\Test\extensions\ClearDead.ext"
-MOVE "ClearDead.zip" "C:\Users\John\Fantasy Grounds Unity\extensions\ClearDead.ext"
